0a5012549239a81577103c7a5ae0fbdb9029bf44
Valentin Lukyanets <valikluks95@gmail.com>
2015-05-11 06:56:23 +0300

    Added SPZ Lab 7

---
+++ Lab7/Lab7/MainForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace LN_Lab2_Registry
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textContainer = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openRecentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textContainer
+            // 
+            this.textContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textContainer.Location = new System.Drawing.Point(0, 24);
+            this.textContainer.MaxLength = 0;
+            this.textContainer.Multiline = true;
+            this.textContainer.Name = "textContainer";
+            this.textContainer.Size = new System.Drawing.Size(805, 347);
+            this.textContainer.TabIndex = 1;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.ShowReadOnly = true;
+            this.openFileDialog.Title = "Select file to open";
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(805, 24);
+            this.menu.TabIndex = 2;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.openRecentToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openRecentToolStripMenuItem
+            // 
+            this.openRecentToolStripMenuItem.Name = "openRecentToolStripMenuItem";
+            this.openRecentToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openRecentToolStripMenuItem.Text = "Open Recent";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 371);
+            this.Controls.Add(this.textContainer);
+            this.Controls.Add(this.menu);
+            this.Name = "MainForm";
+            this.Text = "Form";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textContainer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openRecentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+
+    }
+}
+
---
+++ Lab7/Lab7/MainForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+using Microsoft.Win32;
+
+namespace LN_Lab2_Registry
+{
+    public partial class MainForm : Form
+    {
+        private String recentKey0 = "Recent0";
+        private String recentKey1 = "Recent1";
+        private String recentKey2 = "Recent2";
+
+        private List<String> recentFiles = new List<String>();
+
+        private void openFile(String _fileName)
+        {
+            try
+            {
+                FileStream fs = new FileStream(_fileName, FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                textContainer.Text = sr.ReadToEnd();
+                sr.Close();
+
+                pushFileNameToRecent(_fileName);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+            } 
+        }
+
+        private void saveFile(String _fileName)
+        {
+            try
+            {
+                FileStream fs = new FileStream(_fileName, FileMode.Create);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(textContainer.Text);
+                sw.Close();
+
+                pushFileNameToRecent(_fileName);
+            }
+            catch (System.IO.IOException)
+            {
+            }
+        }
+
+        private void saveRecentFileListToRegistry()
+        {
+            RegistryKey hk = Registry.CurrentUser;
+            RegistryKey hkProgramKey = hk.OpenSubKey("TEXTEDITOR", true);
+            if (hkProgramKey == null)
+                hkProgramKey = hk.CreateSubKey("TEXTEDITOR");
+
+            try
+            {
+                setRecentFileName(hkProgramKey, recentKey0, recentFiles[0]);
+                setRecentFileName(hkProgramKey, recentKey1, recentFiles[1]);
+                setRecentFileName(hkProgramKey, recentKey2, recentFiles[2]);
+            }
+            catch (ArgumentOutOfRangeException e)
+            { }
+
+            refillGuiRecentFileList();
+        }
+
+        private void pushFileNameToRecent(String _fileName)
+        {
+            recentFiles.Insert(0, _fileName);
+            recentFiles = recentFiles.Take(3).ToList();
+
+            saveRecentFileListToRegistry();
+        }
+
+        private String getRecentFileName(RegistryKey _rk, String _key)
+        {
+            return (String)_rk.GetValue(_key, null);
+        }
+
+        private void setRecentFileName(RegistryKey _rk, String _key, String _fileName)
+        {
+            _rk.SetValue(_key, _fileName);
+        }
+
+        private void recentFileItemClickHandler(object _sender, EventArgs _e)
+        {
+            String fileName = (String)((_sender as ToolStripItem).Text);
+            openFile(fileName);
+        }
+
+        private void refillGuiRecentFileList()
+        {
+            openRecentToolStripMenuItem.DropDownItems.Clear();
+            foreach (String recentFileName in recentFiles)
+            {
+                var recentFileItem = openRecentToolStripMenuItem.DropDownItems.Add(recentFileName);
+                recentFileItem.Click += recentFileItemClickHandler;
+            }
+        }
+
+        private void fillRecentFileList()
+        {
+            RegistryKey hk = Registry.CurrentUser;
+            RegistryKey hkProgramKey = hk.OpenSubKey("TEXTEDITOR");
+            if(hkProgramKey == null)
+                hkProgramKey = hk.CreateSubKey("TEXTEDITOR");
+
+            recentFiles.Clear();
+
+            String recent0 = getRecentFileName(hkProgramKey, recentKey0);
+            String recent1 = getRecentFileName(hkProgramKey, recentKey1);
+            String recent2 = getRecentFileName(hkProgramKey, recentKey2);
+
+            if (recent0 != null)
+                recentFiles.Add(recent0);
+
+            if (recent1 != null)
+                recentFiles.Add(recent1);
+
+            if (recent2 != null)
+                recentFiles.Add(recent2);
+
+            refillGuiRecentFileList();
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            fillRecentFileList();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.InitialDirectory.Length == 0)
+                openFileDialog.InitialDirectory = @"C:\";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                openFile(openFileDialog.FileName);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.InitialDirectory.Length == 0)
+                saveFileDialog.InitialDirectory = @"C:\";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                saveFile(saveFileDialog.FileName);
+        }
+    }
+}
